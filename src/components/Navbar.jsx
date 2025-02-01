e7b12b72c3286eb0138783d060af8ff8b9604f2b
Lucifer25x <lucifer25x@protonmail.com>
2025-02-01 17:37:34 +0400

    Rewrite the app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,47 @@
-import { RiSearchLine, RiHeartLine, RiShoppingCartLine, RiUserLine, RiMenuLine, RiCloseLine, RiMoonLine, RiSunLine } from "@remixicon/react"
-import { Link, NavLink } from "react-router-dom"
-import { useState, useContext, useEffect } from "react"
-import { ThemeContext } from "../context/ThemeContext"
+// Import necessary libraries
+import { Link } from "react-router-dom";
+import { RiSearchLine, RiHeartLine, RiShoppingBagLine, RiUserLine, RiMenuLine } from "@remixicon/react";
+
+// Import styles
+import "../assets/styles/components/Navbar.css";
+
+// Import logo
+import logo from "../assets/images/logo.avif";
 
 const Navbar = () => {
-    const [theme, toggleTheme] = useContext(ThemeContext);
-
-    const [open, setOpen] = useState(false);
-    const toggleMenu = () => {
-        setOpen(!open);
-    }
-
-    useEffect(() => {
-        if(open){
-            document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'auto';
-        }
-
-        if (theme === 'dark') {
-            document.body.classList.add('dark');
-        } else {
-            document.body.classList.remove('dark');
-        }
-    }, [theme, open]);
-
     return (
         <>
             <nav>
-                <div className="logo">
-                    <Link to="/">Kafka</Link>
-                </div>
-                <div className="search">
-                    <input type="text" placeholder="Search for anything..." />
-                    <button><RiSearchLine size={25} /></button>
-                </div>
-                <div className="right">
-                    <div>
-                        <NavLink to="/"><RiHeartLine /></NavLink>
-                        <span>2</span>
+                <div className="container">
+                    <div className="logo">
+                        <Link to={"/"}>
+                            <img src={logo} alt="The Record Hub" width={130} />
+                        </Link>
                     </div>
-                    <div>
-                        <NavLink to="/"><RiShoppingCartLine /></NavLink>
-                        <span>3</span>
+                    <div className="search">
+                        <input type="text" placeholder="Search" />
+                        <button><RiSearchLine size={25} /></button>
                     </div>
-                    <button onClick={toggleTheme}>{theme === 'light' ? <RiMoonLine/> : <RiSunLine/>}</button>
-                    <div>
-                        <NavLink to="/"><RiUserLine /></NavLink>
+                    <div className="buttons">
+                        <div className="links">
+                            <Link to={"/wishlist"}>
+                                <RiHeartLine size={25} />
+                                <span>0</span>
+                            </Link>
+                            <Link to={"/cart"}>
+                                <RiShoppingBagLine size={25} />
+                                <span>0</span>
+                            </Link>
+                            <Link to={"/login"}>
+                                <RiUserLine size={25} />
+                            </Link>
+                        </div>
+                        <div className="menu">
+                            <RiMenuLine size={25} />
+                        </div>
                     </div>
-                    <button onClick={toggleMenu}><RiMenuLine /></button>
                 </div>
             </nav>
-
-            <div className={`sidebar_menu ${open ? 'open' : ''}`}>
-                <button onClick={toggleMenu} className="close"><RiCloseLine size={30} /></button>
-                <div className="search">
-                    <input type="text" placeholder="Search for anything..." />
-                    <button><RiSearchLine size={25} /></button>
-                </div>
-                <div className="menu">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/products">Products</NavLink>
-                    <NavLink to="/contact">Contact</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                </div>
-                
-            </div>
         </>
     )
 }
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-    return (
-        <div style={{ height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <h1>404 Not Found</h1>
-        </div>
-    );
-}
-
-export default NotFound;
