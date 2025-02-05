561147210abfc0644bc71b8ec5510c25c6da1e4e
Lucifer25x <lucifer25x@protonmail.com>
2025-02-05 15:52:25 +0400

    Add multi-language structure, Fix some theme related issues, Create Terms of Service Page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
 // Import libraries
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { RiCloseLine, RiArrowDropUpLine } from "@remixicon/react";
+import { LangContext } from "../context/LangContext";
 
 // Import styles
 import "../assets/styles/components/Sidebar.css";
 
 const Sidebar = ({ sidebar, handleSidebar }) => {
+    const { lang, toggleLang } = useContext(LangContext);
     const [help, setHelp] = useState(false);
+
     return (
         <div className="sidebar" style={{ right: sidebar ? "0" : "-100%" }}>
             <div className="close">
@@ -24,6 +27,10 @@ const Sidebar = ({ sidebar, handleSidebar }) => {
                     <Link to={"/faq"}>FAQ</Link>
                     <Link to={"/contact"}>CONTACT</Link>
                 </div>
+            </div>
+
+            <div className="language">
+                <button onClick={toggleLang}>{lang == "EN" ? "AZ" : "EN"}</button>
             </div>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import "./assets/styles/index.css"
 
 // Import context providers
 import { ThemeProvider } from "./context/ThemeContext"
+import { LangProvider } from "./context/LangContext.jsx"
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <LangProvider>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </LangProvider>
     </StrictMode>,
 )
