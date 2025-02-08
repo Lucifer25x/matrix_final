6221760fa835d57178a9a2e3f0e839bc87770929
Lucifer25x <lucifer25x@protonmail.com>
2025-02-08 20:16:21 +0400

    Add to cart

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,26 +1,43 @@
 // Import libraries
 import { RiHeartLine } from "@remixicon/react";
+import { useCart } from "react-use-cart";
+import { toast, Bounce } from "react-toastify";
 
 // Import styles
 import "../assets/styles/components/Product.css";
 import { Link } from "react-router-dom";
 
-const Product = ({ id, img, title, desc, price }) => {
+const Product = ({ product }) => {
+    const { addItem } = useCart();
+
+    const handleAddToCart = () => {
+        addItem(product);
+
+        toast.success("Product was added to your cart!", {
+            position: "bottom-center",
+            autoClose: 3000,
+            closeOnClick: false,
+            theme: "colored",
+            transition: Bounce,
+        });
+    }
+
     return (
         <div className="product">
+
             <div className="img">
-                <img src={img} alt={title} />
+                <img src={product.img} alt={product.title} />
                 <RiHeartLine size={30} />
-                <div className="add">
+                <div className="add" onClick={handleAddToCart}>
                     <p>ADD TO CART</p>
                 </div>
             </div>
             <div className="info">
-                <Link to={`/product/${id}`}>
-                    <h3>{title}</h3>
+                <Link to={`/product/${product.id}`}>
+                    <h3>{product.title}</h3>
                 </Link>
-                <p>{desc}</p>
-                <p>${price}</p>
+                <p>{product.desc}</p>
+                <p>${product.price}</p>
             </div>
         </div>
     )
