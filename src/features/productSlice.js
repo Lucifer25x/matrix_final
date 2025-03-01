7daf03bc57ab794af1a4be70f6603eca9ceecc7a
Lucifer25x <lucifer25x@protonmail.com>
2025-03-01 19:51:05 +0400

    Update

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -7,29 +7,12 @@ const initialState = {
     loading: false
 }
 
-// FIXME: Fix the updateProduct reducer
 const productSlice = createSlice({
     name: "products",
     initialState,
     reducers: {
         setProducts: (state, action) => {
             state.products = action.payload;
-        },
-        addProduct: (state, action) => {
-            state.products.push(action.payload);
-        },
-        removeProduct: (state, action) => {
-            state.products = state.products.filter(product => product.id !== action.payload);
-        },
-        updateProduct: (state, action) => {
-            // const { id, name, price, description, image } = action.payload;
-            // const existingProduct = state.products.find(product => product.id == id);
-            // if (existingProduct) {
-            //     existingProduct.name = name;
-            //     existingProduct.price = price;
-            //     existingProduct.description = description;
-            //     existingProduct.image = image;
-            // }
         }
     }
 })
---
+++ src/hooks/useProduct.js
@@ -0,0 +1,98 @@
+// Import libraries
+import { useEffect, useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../features/productSlice";
+import { UserContext } from "../context/UserContext";
+import supabase from "../utils/supabase";
+
+// Custom useProduct hook
+const useProduct = () => {
+    const { user, loading } = useContext(UserContext);
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.products.products);
+
+    // Fetch products
+    useEffect(() => {
+        if (!loading && user) {
+            const fetchProducts = async () => {
+                const { data, error } = await supabase
+                    .from("products")
+                    .select("*");
+
+                if (error) console.error(error);
+                else dispatch(setProducts(data));
+            }
+
+            fetchProducts();
+        }
+    }, [user, loading, dispatch])
+
+    // Add a product
+    const addProductData = async (data) => {
+        const { error } = await supabase
+            .from("products")
+            .insert(data);
+
+        if (error) {
+            console.error(error);
+        } else {
+            const { data: updatedData, error: fetchError } = await supabase
+                .from("products")
+                .select("*");
+
+            if (fetchError) {
+                console.error(fetchError);
+            } else {
+                dispatch(setProducts(updatedData));
+            }
+        }
+    }
+
+    // Update the product
+    const updateProductData = async (id, data) => {
+        const { error } = await supabase
+            .from("products")
+            .update(data)
+            .eq("id", id);
+
+        if (error) {
+            console.error(error);
+        } else {
+            const { data: updatedData, error: fetchError } = await supabase
+                .from("products")
+                .select("*");
+
+            if (fetchError) {
+                console.error(fetchError);
+            } else {
+                dispatch(setProducts(updatedData));
+            }
+        }
+    }
+
+    // Remove a product
+    const removeProductData = async (id) => {
+        const { error } = await supabase
+            .from("products")
+            .delete()
+            .eq("id", id);
+
+        if (error) {
+            console.error(error);
+        } else {
+            const { data: updatedData, error: fetchError } = await supabase
+                .from("products")
+                .select("*");
+
+            if (fetchError) {
+                console.error(fetchError);
+            } else {
+                dispatch(setProducts(updatedData));
+            }
+        }
+    }
+
+    return { products, addProductData, updateProductData, removeProductData };
+}
+
+export default useProduct;
