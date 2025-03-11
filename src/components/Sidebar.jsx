c23f4899938201652e5df34f3d870ce1f70bd539
Lucifer25x <lucifer25x@protonmail.com>
2025-03-11 21:53:28 +0400

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -44,7 +44,6 @@ const Sidebar = ({ sidebar, handleSidebar }) => {
                 <Link to={"/about"}><StaticLang en="ABOUT US" az="HAQQIMIZDA" /></Link>
                 <Link to={"/products"}><StaticLang en="PRODUCTS" az="MƏHSULLAR" /></Link>
                 <Link to={"/blogs"}><StaticLang en="BLOGS" az="BLOGLAR" /></Link>
-                {/* <Link to={"/sales"}><StaticLang en="SALE" az="ENDİRİMLƏR" /></Link> */}
                 <p onClick={() => setHelp(!help)} className={help ? '' : 'open'}><StaticLang en="HELP" az="YARDIM" /> <RiArrowDropUpLine size={30} /></p>
                 <div className={`help ${help ? "active" : ""}`}>
                     <Link to={"/faq"}>FAQ</Link>
